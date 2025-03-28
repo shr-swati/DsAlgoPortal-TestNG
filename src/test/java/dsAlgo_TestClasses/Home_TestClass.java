03451893c0c0ab7e60d287c8f8c9ae8ce302025b
Swati Shrivastava <shr.swati1910@gmail.com>
2025-03-28 08:35:42 -0500

    initial commit

---
+++ src/test/java/dsAlgo_TestClasses/Home_TestClass.java
@@ -0,0 +1,105 @@
+package dsAlgo_TestClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import dsAlgo_DriverFactory.DriverFactory;
+import dsAlgo_PageFactory.Home_PageFactory;
+import dsAlgo_Utilities.ListenersReporter;
+import dsAlgo_Utilities.LoggerReader;
+
+@Listeners(dsAlgo_Utilities.ListenersReporter.class)
+
+public class Home_TestClass {
+	
+	WebDriver driver;
+	Home_PageFactory homePage;
+	public static final String expectedHomeMessage = "You are not logged in";
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browser) {
+        DriverFactory driverFactory = new DriverFactory();
+        driverFactory.setUp(browser); 
+        driver = DriverFactory.getDriver();
+        
+        homePage = new Home_PageFactory();
+		homePage.launchUrl();
+		LoggerReader.info("=========URL is launched========");
+		homePage.getStartedHomeBtnClick();
+    }
+
+	@Test(priority = 1)
+	public void arraysClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.arraysBtnClick();
+		assertMessage();
+		LoggerReader.info("Arrays option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	@Test(priority = 2)
+	public void linkedListClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.linkedListBtnClick();
+		assertMessage();
+		LoggerReader.info("Linked List option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	@Test(priority = 3)
+	public void stackClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.stackBtnClick();
+		assertMessage();
+		LoggerReader.info("Stack option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	@Test(priority = 4)
+	public void queueClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.queueBtnClick();
+		assertMessage();
+		LoggerReader.info("Queue option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	@Test(priority = 5)
+	public void treeClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.treeBtnClick();
+		assertMessage();
+		LoggerReader.info("Tree option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	@Test(priority = 6)
+	public void graphClick() {
+		homePage.dataStructureDropDownClick();
+		homePage.graphBtnClick();
+		assertMessage();
+		LoggerReader.info("Graph option under Data Structures drop-down is clicked in HomePage");
+	}
+
+	public void assertMessage() {
+		String homeText = homePage.getHomeLogMessage();
+		Assert.assertEquals(homeText, expectedHomeMessage);
+	}
+	
+	@AfterMethod
+    public void tearDown() {
+	    DriverFactory driverFactory = new DriverFactory();
+	    driverFactory.tearDown(); 
+	}
+	
+	@AfterMethod
+	public void takeScreenshotOnFailure(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE && driver != null) {
+			LoggerReader.info("Test failed: " + result.getName() + ". Attaching screenshot to Allure.");
+			ListenersReporter.attachScreenshotToAllure(driver);
+		   }
+	  }	
+}
---
+++ src/test/java/dsAlgo_TestClasses/Login_TestClass.java
@@ -0,0 +1,73 @@
+package dsAlgo_TestClasses;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import dsAlgo_BaseClass.BaseClass;
+import dsAlgo_PageFactory.Home_PageFactory;
+import dsAlgo_PageFactory.Login_PageFactory;
+import dsAlgo_Utilities.DataProviderClass;
+import dsAlgo_Utilities.LoggerReader;
+
+@Listeners(dsAlgo_Utilities.ListenersReporter.class)
+
+public class Login_TestClass extends BaseClass {
+
+	Home_PageFactory homePage;
+	Login_PageFactory loginPage;
+
+	@BeforeMethod
+	public void initPageObjects() {
+		homePage = new Home_PageFactory();
+		loginPage = new Login_PageFactory();
+		homePage.launchUrl();
+		homePage.getStartedHomeBtnClick();
+	}
+	
+	@Test(priority = 1, dataProvider = "validLoginData", dataProviderClass = DataProviderClass.class)
+	public void validLogInData(String username, String password, String message) {
+		loginPage.signInClick();
+		loginPage.userNameLoginBtn.sendKeys(username);
+		loginPage.passwordLoginBtn.sendKeys(password);
+		loginPage.LoginBtnClick();
+		String loggedInMsg = loginPage.loggedInMessage.getText();
+		Assert.assertEquals(loggedInMsg, message);
+		LoggerReader.info("User enter valid login credentials");
+	} 
+
+	@Test(priority = 2, dataProvider = "logOutData", dataProviderClass = DataProviderClass.class)
+	public void loginLogOut(String username, String password, String message) {
+		loginPage.signInClick();
+		loginPage.userNameLoginBtn.sendKeys(username);
+		loginPage.passwordLoginBtn.sendKeys(password);
+		loginPage.LoginBtnClick();
+		loginPage.signOutBtnClick();
+		String loggedOutMsg = loginPage.loggedOutMessage.getText();
+		Assert.assertEquals(loggedOutMsg, message);
+		LoggerReader.info("User logs out");
+	}
+	@Test(priority = 3, dataProvider = "invalidLoginData", dataProviderClass = DataProviderClass.class)
+	public void invalidLogIn(String username, String password, String message) {
+		loginPage.signInClick();
+	    loginPage.userNameLoginBtn.sendKeys(username);
+		loginPage.passwordLoginBtn.sendKeys(password);
+		loginPage.LoginBtnClick(); 
+		String invalidMsg = loginPage.invalidMessage.getText();
+		Assert.assertEquals(invalidMsg, message);
+		LoggerReader.info("User enter invalid login credentials");
+	}
+	
+	@Test(priority = 4, dataProvider = "loginBlank", dataProviderClass = DataProviderClass.class)
+	public void loginBlankFields(String username, String password, String message) throws InterruptedException {
+		loginPage.signInClick();
+	    loginPage.userNameLoginBtn.sendKeys(username);
+		loginPage.passwordLoginBtn.sendKeys(password);
+		loginPage.LoginBtnClick(); 
+		String actualmessage = loginPage.getPopUpMessage();
+		Assert.assertEquals(actualmessage, message,"message dont match");
+		LoggerReader.info("User enter blank credentials");
+	}
+
+}
